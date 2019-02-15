44ff2fb9c1af9358b57ba49463ad5c181a2344d6
Jeff Bahns <bahns@sonoma.edu>
2019-02-15 15:39:58 +0530

    backend changes, working but needs some more before finished

--- Server/app.js
+++ Server/app.js
@@ -7,45 +7,116 @@ const index = require("./routes/index");
 const app = express();
 app.use(index);
 const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
+const io = socketIo(server);
 
 
 var users = [];
 
-io.on("connection", socket => {
+var usersObj = {
+  
+};
+
+const getUserID = (username) => {
+  var timestamp = new Date().getTime();
+  var usercode = username.split('').map(l => l.charCodeAt(0)).reduce((ret, code) => ret + code, '');
+  var combined = timestamp + usercode;
+  
+  // probably add in more variation, like initial socket #'s
+  uid = combined;
+  return uid;
+};
+
+const removeUser = (uid) => {
+  var userToRemove = users.find(user => user.uid === uid);
+  var index = users.indexOf(userToRemove);
+  return users.splice(index, 1);
+};
+
+const room = (namespace) => {
+  
+  var chat = io.of(namespace).on("connection", socket => {
+    // socket.broadcast.emit('user-connect', {username: socket.username});
+    // socket.emit('new-message', {message: `Welcome to the ${namespace} chatroom!`, username: 'Administrator'});
+
+    socket.on("userLogin", (user) => {
+      // todo: look for inactive users, 'sleeping'
+
+      if (user.uid && usersObj[user.uid]) { // user already logged in, same browser or whatever
+        socket.emit("userLoginResponse", {users: users})
+      } else {
+        var user = {...user, uid: getUserID(user.username)};
+        socket.username = user.username;
+        socket.uid = user.uid;
+
+        users.push(user);
+        socket.emit("userLoginResponse", {users: users})
+        socket.broadcast.emit('userConnect', {user: user}); // tell others about new user
+      }
+
+      
+    });
+  
+    socket.on("newMessage", (data) => {
+      socket.broadcast.emit('newMessage', {...data, username: socket.username});
+    });
+    
+    socket.on("disconnect", () =>  {
+      socket.broadcast.emit('userDisconnect', {user: removeUser(socket.uid), users: users});
+      // socket.broadcast.emit('new-message', {message: `${socket.username} has left the chat`, username: 'Administrator'});
+    });
+
+  });
+
+};
+
+
+// room('/admin');
+room('/general');
+
+
+/* io.on("connection", socket => {
 
   socket.username = 'anonymous';
 
+  socket.emit('new-message', {message: 'Welcome to my chat room', username: 'Administrator'});
+  socket.broadcast.emit('new-message', {message: `${socket.username} joined the room!`, username: 'Administrator'});
+
+  
+
   socket.on("register-username", (data) => {
+    let previous = socket.username; 
     let requested_username = data.username;
-    
+
     if (users.indexOf(requested_username) > -1) { // username taken
       // console.log('username taken');
       socket.emit("username-register-failure", {response: 'Username already used'});
     } else { 
       // console.log('register username', data.username);
       socket.username = data.username;
+      
       users.push(requested_username);
+      
       socket.emit("username-register-success", {response: 'Username add success'});
+      io.sockets.emit("new-message", {message: `${previous} changed their name to ${socket.username}`, username: 'Administrator'});
     }
     
-  });
-
-  socket.on("get-users", () => {
-    console.log(users);
   });
 
   socket.on("new-message", (data) => {
     io.sockets.emit('new-message', {message: data.message, username: socket.username});
   });
+  
+  socket.on("disconnect", () =>  {
+    console.log("Client disconnected");
+    socket.broadcast.emit('new-message', {message: 'disconnect', username: 'Administrator'});
+  });
 
-  socket.on("disconnect", () => console.log("Client disconnected"));
+  // io.sockets.emit('hi', 'everyone');
+}); */
 
 
 
-});
-
-const getApiAndEmit = async socket => {
+/* const getApiAndEmit = async socket => {
   let api = "https://api.darksky.net/forecast/d07d0a17f98a1feaf6a843fb9de2863d/43.7695,11.2558";
   let api2 = "http://www.spitcast.com/api/county/spots/santa-cruz/";
   try {
@@ -58,5 +129,7 @@ const getApiAndEmit = async socket => {
   } catch (error) {
     console.error(`Error: ${error}`);
   }
-};
+}; */
+
+
 server.listen(port, () => console.log(`Listening on port ${port}`));
