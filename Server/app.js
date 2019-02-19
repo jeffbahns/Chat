d88933855cc68383cc6f8fe0c76093964d0b64a3
Jeff Bahns <bahns@sonoma.edu>
2019-02-19 11:24:02 +0530

    removed old server code, modified login return object

--- Server/app.js
+++ Server/app.js
@@ -42,15 +42,19 @@ const room = (namespace) => {
       // todo: look for inactive users, 'sleeping'
 
       if (user.uid && usersObj[user.uid]) { // user already logged in, same browser or whatever
-        socket.emit("userLoginResponse", {users: users})
+        socket.emit("userLoginResponse", {users: users});
       } else {
-        var user = {...user, uid: getUserID(user.username)};
+        p('receiving for login...')
+        p(user);
+        var user = {...user, uid: user.uid || getUserID(user.username)}; // assign a user id if first login
         socket.username = user.username;
         socket.uid = user.uid;
 
         users.push(user);
-        socket.emit("userLoginResponse", {users: users})
+        socket.emit("userLoginResponse", {users: users, user: user})
         socket.broadcast.emit('userConnect', {user: user}); // tell others about new user
+        console.log('logging in...');
+        console.log(user);
       }
 
       
@@ -68,67 +72,6 @@ const room = (namespace) => {
 
 };
 
-
-// room('/admin');
 room('/general');
-
-
-/* io.on("connection", socket => {
-
-  socket.username = 'anonymous';
-
-  socket.emit('new-message', {message: 'Welcome to my chat room', username: 'Administrator'});
-  socket.broadcast.emit('new-message', {message: `${socket.username} joined the room!`, username: 'Administrator'});
-
-  
-
-  socket.on("register-username", (data) => {
-    let previous = socket.username; 
-    let requested_username = data.username;
-
-    if (users.indexOf(requested_username) > -1) { // username taken
-      // console.log('username taken');
-      socket.emit("username-register-failure", {response: 'Username already used'});
-    } else { 
-      // console.log('register username', data.username);
-      socket.username = data.username;
-      
-      users.push(requested_username);
-      
-      socket.emit("username-register-success", {response: 'Username add success'});
-      io.sockets.emit("new-message", {message: `${previous} changed their name to ${socket.username}`, username: 'Administrator'});
-    }
-    
-  });
-
-  socket.on("new-message", (data) => {
-    io.sockets.emit('new-message', {message: data.message, username: socket.username});
-  });
-  
-  socket.on("disconnect", () =>  {
-    console.log("Client disconnected");
-    socket.broadcast.emit('new-message', {message: 'disconnect', username: 'Administrator'});
-  });
-
-  // io.sockets.emit('hi', 'everyone');
-}); */
-
-
-
-/* const getApiAndEmit = async socket => {
-  let api = "https://api.darksky.net/forecast/d07d0a17f98a1feaf6a843fb9de2863d/43.7695,11.2558";
-  let api2 = "http://www.spitcast.com/api/county/spots/santa-cruz/";
-  try {
-    const res = await axios.get(
-      api2
-    ); // Getting the data from DarkSky
-    // console.log("FromAPI", res.data);
-    socket.emit("FromAPI", res.data.length + " spots in the system"); // Emitting a new message. It will be consumed by the client
-
-  } catch (error) {
-    console.error(`Error: ${error}`);
-  }
-}; */
-
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
