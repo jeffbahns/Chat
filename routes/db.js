794f1aeb26706b614433c0ab3b6e78baaf5d7585
Jeff Bahns <bahns@sonoma.edu>
2019-03-13 17:48:55 +0530

    Switched to postgresql for this project so that i can host my db through heroku, did the initial setup and wrote some of the API for my new database

--- routes/db.js
+++ routes/db.js
@@ -1,39 +1,69 @@
 var mysql = require('mysql'),
+    pg = require('pg'),
     useDB = false,
-    db,
-    con;
+    postgres
+;
 
 if (process.env.DB_HOST) { // checking on .env, to determine whether to run database functionality
-    useDB = true;
-    db = require("./db");
-    con = mysql.createConnection({
+    useDB = true
+    postgres = new pg.Pool({
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_DATABASE,
-        debug: false
     });
+    // useDB = false;
 }
 
 
-/* 'messages' table handlers */
-module.exports.addMessage = (message, uid) => {
+/* users' handlers postgres */
+
+module.exports.getUsers = (callback) => {
+    // if ( ! useDB ) { return ; }
+
+    postgres.query('SELECT * FROM Users', (err, res) => {
+        console.log(res.rows);
+    });
+}
+
+module.exports.createUser = (uid, username) => {
+    let query = `INSERT INTO "users" ( "uid", "username") VALUES (\'${uid}\', \'${username}\');`;
+    postgres.query(query, (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(res.rows);
+        }
+    });
+}
+// this.createUser('dfjekfjkqwjef', 'jeffbizzle')
+
+
+
+
+
+
+
+/* 'messages' table handlers for postgres */
+
+module.exports.createMessage = (message, uid) => {
     if ( ! useDB ) { return ; }
-    
-    con.query(`INSERT INTO messages (uid, dbid, message, ts) VALUES (\'${uid}\', 0, \'${message}\', NOW());`,
-        (err, rows) => {
-            if (err) {
-                console.log(err);
-            }
+    let query = `INSERT INTO messages (uid, dbid, message, ts) VALUES (\'${uid}\', 1, \'${message}\', NOW());`;
+    postgres.query(query, (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
             console.log(rows);
-        });
+        }
+    });
 };
+this.createMessage('hello world!', 'dfjekfjkqwjef');
 
 module.exports.getMessages = (callback) => {
     if ( ! useDB ) { return callback([]); }
 
-    con.query('SELECT * FROM messages ORDER BY ts DESC, id DESC;', 
+    postgres.query('SELECT * FROM messages ORDER BY ts DESC, id DESC;', 
         (err, rows) => {
             if (err) {
                 return callback([]);
